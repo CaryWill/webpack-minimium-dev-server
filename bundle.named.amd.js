39b5fc6db13a0c9fad702807fddfcc83b959e0a6
wangzhangsheng(cary) <wb-wzs728893@alibaba-inc.com>
2022-09-08 20:45:20 +0800

    fix: update

--- bundle.named.amd.js
+++ bundle.named.amd.js
@@ -1,4 +1,4 @@
-define("MyLibrary", [], function() { return /******/ (function(modules) { // webpackBootstrap
+define([], function() { return /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
 /******/
